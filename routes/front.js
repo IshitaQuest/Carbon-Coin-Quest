26adf4904dfb4f399637c0508cf805cb972a4205
prashant barge <prashantbarge22@gmail.com>
2022-01-13 13:11:04 +0530

    issue resolved

--- routes/front.js
+++ routes/front.js
@@ -925,7 +925,6 @@ router.get("/company-dashboard",isFirmLoggedIn,(req,res)=>{
  router.post("/firmlogin",(req,res)=>{
   DecarbonFirmModel.findOne({email:req.body.email,password:req.body.password}).then(result=>{
     if(result==null){
-        alert("Wrong Password or Email");
         res.redirect("/#about");
     }else{
       req.session.user = result;
@@ -935,14 +934,13 @@ router.get("/company-dashboard",isFirmLoggedIn,(req,res)=>{
       })
     }
   }).catch(err=>{
-    res.render("register-form.ejs",{err_msg:err.message});
+    res.redirect("/#about");
   })
  });
 
  router.post("/decarbinationCompanyLogin",(req,res)=>{
   DecarbonCompanyModel.findOne({email:req.body.email,password:req.body.password}).then(result=>{
     if(result==null){
-      alert("Wrong Password or Email");
       res.redirect("/#about");
     }else{
       req.session.user = result;
@@ -952,7 +950,7 @@ router.get("/company-dashboard",isFirmLoggedIn,(req,res)=>{
       })
     }
   }).catch(err=>{
-    res.render("register-tree-form",{err_msg:err.message});
+      res.redirect("/#about");
   })
 
  })
